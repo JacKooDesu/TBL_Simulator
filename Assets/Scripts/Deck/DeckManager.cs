258563e011823c37083bd73c3f4625e02399e774
LAPTOP-I50KRMDP\User <kuo741258@gmail.com>
2021-05-10 13:09:34 +0800

    fix card id not match to color

--- Assets/Scripts/Deck/DeckManager.cs
+++ Assets/Scripts/Deck/DeckManager.cs
@@ -13,7 +13,6 @@ namespace TBL
 
         private void OnEnable()
         {
-            int counter = 0;
             for (int i = 0; i < deck.CardConfigs.Count; ++i)
             {
                 for (int j = 0; j < deck.CardConfigs[i].ColorConfigs.Count; ++j)
@@ -22,11 +21,10 @@ namespace TBL
                     {
                         CardSetting temp = deck.CardConfigs[i].Card;
                         Debug.Log((temp.ID & (ushort)CardSendType.Direct) == (ushort)CardSendType.Direct);
-                        CardSetting card = new CardSetting();
-                        card.ID = temp.ID;
-                        card.CardColor = deck.CardConfigs[i].ColorConfigs[j].Color;
-                        GameObject g = new GameObject(card.GetCardNameFully() + counter.ToString());
-                        counter++;
+                        GameObject g = new GameObject();
+                        CardSetting card = g.AddComponent<CardSetting>();
+                        card.ID = (ushort)(temp.ID + (ushort)(deck.CardConfigs[i].ColorConfigs[j].Color));
+                        g.name = card.GetCardNameFully();
                     }
 
                 }
