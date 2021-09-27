c98747941f7a41869836a6853c07407811ff5854
DESKTOP-0TBJVPI\JacKoo <kuo741258@gmail.com>
2021-09-27 23:32:48 +0800

    新增 Command腳本，後續須新增help command

---
+++ Assets/Scripts/Command/CommandBase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace TBL.Command
+{
+    public class CommandBase
+    {
+        string commandId;
+        public string CommandId { get => commandId; }
+
+        public CommandBase(string id)
+        {
+            commandId = id;
+        }
+    }
+
+    public class Command : CommandBase
+    {
+        Action command;
+        public Command(string id, Action command) : base(id)
+        {
+            this.command = command;
+        }
+
+        public void Invoke()
+        {
+            command.Invoke();
+        }
+    }
+
+    public class Command<T1> : CommandBase
+    {
+        Action<T1> command;
+        public Command(string id, Action<T1> command) : base(id)
+        {
+            this.command = command;
+        }
+
+        public void Invoke(T1 value)
+        {
+            command.Invoke(value);
+        }
+    }
+
+    public class Command<T1, T2> : CommandBase
+    {
+        Action<T1, T2> command;
+        public Command(string id, Action<T1, T2> command) : base(id)
+        {
+            this.command = command;
+        }
+
+        public void Invoke(T1 v1, T2 v2)
+        {
+            command.Invoke(v1, v2);
+        }
+    }
+
+}
