445ce7e494a7cdfd2c492665be0f85a813d7aa06
DESKTOP-0TBJVPI\JacKoo <kuo741258@gmail.com>
2022-01-12 19:58:56 +0800

    因為太閒所以更完善了RichTextHelper

--- Assets/Scripts/Utils/RichTextHelper.cs
+++ Assets/Scripts/Utils/RichTextHelper.cs
@@ -4,6 +4,65 @@ using UnityEngine;
 
 public class RichTextHelper
 {
+    public enum Style
+    {
+        Color,
+        Bold,
+        Italics
+    }
+
+    public class SettingBase
+    {
+
+        public Style style;
+
+        public SettingBase(Style style)
+        {
+            this.style = style;
+        }
+    }
+
+    public class Setting<T> : SettingBase
+    {
+        public T suffix;
+
+        public Setting(Style style, T suffix) : base(style)
+        {
+            this.style = style;
+            this.suffix = suffix;
+        }
+    }
+
+    public static string TextWithStyles(string s, params SettingBase[] settings)
+    {
+        string final = s;
+        List<SettingBase> sList = new List<SettingBase>(settings);
+
+        if (sList.Find((s) => s.style == Style.Bold) != null)
+            final = TextWithBold(final);
+
+        if (sList.Find((s) => s.style == Style.Italics) != null)
+            final = TextWithItalics(final);
+
+        if (sList.Find((s) => s.style == Style.Color) != null)
+        {
+            var setting = sList.Find((s) => s.style == Style.Color) as Setting<Color>;
+            final = TextWithColor(final, setting.suffix);
+        }
+
+        return final;
+    }
+
+    public static string TextWithItalics(string s)
+    {
+        return $"<i>{s}</i>";
+    }
+
+    public static string TextWithBold(string s)
+    {
+        return $"<b>{s}</b>";
+    }
+
     public static string TextWithColor(string s, Color c)
     {
         string final = "";
