212ddd01c83c7839ec5a60529d0870ffdfd1c3d4
DESKTOP-0TBJVPI\JacKoo <kuo741258@gmail.com>
2022-01-12 17:51:52 +0800

    避免後續RichText程式碼冗長，先建立RichTextHelper，之後Log系統也可以使用。

---
+++ Assets/Scripts/Utils/RichTextHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RichTextHelper
+{
+    public static string TextWithColor(string s, Color c)
+    {
+        string final = "";
+        final += $"<color=#{ColorUtility.ToHtmlStringRGBA(c)}>";
+        final += s;
+        final += "</color>";
+        return final;
+    }
+}
