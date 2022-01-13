22813d8dfbc652d1ea037056cc7923674b611df6
DESKTOP-0TBJVPI\JacKoo <kuo741258@gmail.com>
2022-01-14 01:21:51 +0800

    Version Text script added

---
+++ Assets/Scripts/Utils/VersionText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class VersionText : MonoBehaviour
+{
+    public Text text;
+    public string githubLink;
+    private void Start()
+    {
+        text.text = $"GitHub\nv {Application.version}";
+        var eventTrigger = gameObject.AddComponent<EventTrigger>();
+        var trigger = new EventTrigger.Entry();
+        trigger.eventID = EventTriggerType.PointerClick;
+        trigger.callback.AddListener((e) =>
+        {
+            Application.OpenURL(githubLink);
+        });
+        eventTrigger.triggers.Add(trigger);
+    }
+}
